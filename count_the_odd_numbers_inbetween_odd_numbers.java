86abd126ea7804fc2b34b4a719d1d7852ad9dcb9
himajaabbina <83061371+himajaabbina@users.noreply.github.com>
2023-06-07 12:12:54 +0530

    import java.util.*; public class oddnum {    public static int findodd(int arr[],int size)    {       int count=0;       for(int i=1;i< size-1;i++)       {          if(arr[i]%2 !=0)          {              if(arr[i-1]%2!=0 && arr[i+1]%2 !=0)                     count++;          }       }       return count;    }    public static void main(String args[])    {       Scanner sc = new Scanner(System.in);       int n,x[];       n = sc.nextInt();       x = new int[n];       for(int i=0;i<n;i++)       x[i] = sc.nextInt();       int result=findodd(x,n);       System.out.println(result);    }
    }

---
+++ count_the_odd_numbers_inbetween_odd_numbers.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class oddnum
+{  
+   public static int findodd(int arr[],int size)
+   {
+      int count=0;
+      for(int i=1;i< size-1;i++)
+      {
+         if(arr[i]%2 !=0)
+         {
+             if(arr[i-1]%2!=0 && arr[i+1]%2 !=0)
+                    count++;
+         }
+      }
+      return count;
+   }
+   public static void main(String args[])
+   {
+      Scanner sc = new Scanner(System.in);
+      int n,x[];
+      n = sc.nextInt();
+      x = new int[n];
+      for(int i=0;i<n;i++)
+      x[i] = sc.nextInt();
+      int result=findodd(x,n);
+      System.out.println(result);
+   }
+     
+}
